3cd0ab13d9a28fd8f1eeab2f1f13400ae8b2a8e2
Александр <matruk.aleksandr@gmail.com>
2022-03-27 16:22:05 +0300

    1-3 задания

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,71 @@
+package ru.skypro;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void metod_1(int year){
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год является високосным!");
+        }
+        else System.out.println(year + " год не является високосным!");
+    }
+
+    public static void metod_2(int clientOS, int clientDeviceYear){
+
+        int currentYear = LocalDate.now().getYear();
+        if (clientDeviceYear <= 2014 || clientDeviceYear < currentYear) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для AndroidOS по ссылке");
+            }
+        } else {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для AndroidOS по ссылке");
+            }
+        }
+    }
+
+    public static void metod_3(int deliveryDistance){
+        int deliveryDay = 1;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + deliveryDay);}
+
+        else {
+            if (deliveryDistance >20 && deliveryDistance < 60) { deliveryDay = deliveryDay+1;
+                System.out.println("Потребуется дней: " + deliveryDay);}
+        }
+
+        if (deliveryDistance >60 && deliveryDistance < 100) { deliveryDay = deliveryDay+2;
+            System.out.println("Потребуется дней: " + deliveryDay);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Задание 1");
+        metod_1(2020);
+        System.out.println();
+
+        System.out.println("Задание 2");
+        metod_2(1, 2015);
+        System.out.println();
+
+        System.out.println("Задание 3");
+        metod_3(55);
+        System.out.println();
+
+        System.out.println("Задание 4");
+
+
+
+
+
+    }
+}
+
